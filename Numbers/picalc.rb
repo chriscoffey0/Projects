14af428b5fbf0ae0b9c91ad7d2712e4cacb227da
Chris Coffey <chriscoffey0@gmail.com>
2013-07-22 14:54:09 -0400

    continue working on pi

--- Numbers/picalc.rb
+++ Numbers/picalc.rb
@@ -6,18 +6,28 @@ def picalc(digits)
 	#Step 1 - define new array of size [10n/3]+1 containing 2s
 	a = Array.new(len).fill(2)
 
-	#define arrays to hold series values
-	n = Array(0..len)
-	d = n.collect {|x| x*2+1}
-
 	#Step 2 - repeat n times
 	digits.times do
 		#multiply each entry of the array by 10
 		a.collect! {|x| x*10}
 
 		#put the array into regular form
+		(1..len).each do |x|
+			print a
+			#calculate quotient
+
+			#calculate remainder
 		
+			#calculate carry
+		
+			#store the remainder in array a (current index)
+		
+			#add the carry to array a (next index)
+	
+		end
 	end
+
+	print a
 end
 
 print "Calc how many digits of Pi: "
