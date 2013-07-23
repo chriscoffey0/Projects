df29c9ff4225b248513aced68163f9808743dca0
Chris Coffey <chriscoffey0@gmail.com>
2013-07-23 10:15:46 -0400

    finished pi calculator

--- Numbers/picalc.rb
+++ Numbers/picalc.rb
@@ -2,6 +2,9 @@
 
 def picalc(digits)
 	len = 10*digits/3+1
+
+	pi_held = Array.new
+	pi_true = ""
 	
 	#Step 1 - define new array of size [10n/3]+1 containing 2s
 	a = Array.new(len).fill(2)
@@ -12,22 +15,46 @@ def picalc(digits)
 		a.collect! {|x| x*10}
 
 		#put the array into regular form
-		(1..len).each do |x|
-			print a
-			#calculate quotient
-
+		(1..(len-1)).each do |x|
+			index = len-x
 			#calculate remainder
-		
+			rem = a[index]%(2*index+1)
+			#calculate quotient
+			quot = (a[index]-rem)/(2*index+1)
 			#calculate carry
-		
+			carry = quot*index
 			#store the remainder in array a (current index)
-		
+			a[index] = rem
 			#add the carry to array a (next index)
-	
+			a[index-1] += carry
+		end
+
+		#get the pre-digit
+		rem = a[0]%10
+		quot = (a[0]-rem)/10
+		#set the 0 index to the remainder
+		a[0] = rem
+		#release the held queue if not 9 or 10
+		if quot < 9
+			pi_held.each {|x| pi_true += x.to_s}
+			pi_held = Array.new
+		end
+		pi_held << quot if quot < 10
+		if quot == 10
+			#increase all held digits by 1 (9 becomes 0)
+			pi_held.collect! {|x| (x+1)%10}
+			#release held digits to true
+			pi_held.each {|x| pi_true += x.to_s}
+			pi_held = Array.new
+			#hold 0 as a predigit
+			pi_held << 0
 		end
 	end
-
-	print a
+	#flush held to true for remaining digits
+	pi_held.each {|x| pi_true += x.to_s}
+	#insert a decimal point
+	pi_true.insert 1,"."
+	return pi_true
 end
 
 print "Calc how many digits of Pi: "
