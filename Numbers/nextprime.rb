331642f66dc77dc978e8f5b239550e74ad9f6fdf
Chris Coffey <chriscoffey0@gmail.com>
2013-07-28 15:21:22 -0400

    added get next prime program

---
+++ Numbers/nextprime.rb
@@ -0,0 +1,31 @@
+def get_next_prime(number)
+	is_prime = false
+	#start looking at the next number
+	number += 1
+	#keep searching till the next prime is found
+	while !is_prime
+		is_prime = true
+		#check to make sure number is only divisible by 1 and self
+		(2..(number-1)).each do |factor|
+			is_prime = false if number%factor == 0
+		end
+		#if this is not prime, check the next number
+		number += 1 if !is_prime
+	end
+	#prime found, so return it
+	return number
+end
+
+#menu loop
+current_prime = 1
+option = 0
+while option != 3
+	puts "Main menu\n====================================="
+	puts "Current Prime is: #{current_prime}\n1) Find Next\n2) Set Start\n3) Exit"
+	option = gets.chomp.to_i
+	current_prime = get_next_prime(current_prime) if option == 1
+	if option == 2
+		print "Enter number to start looking from: "
+		current_prime = gets.chomp.to_i
+	end
+end
